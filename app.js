a97563a57d89f5acb5c1eead38510a8cbe92bda2
samroodAli <devsamrood@gmail.com>
2021-06-22 20:20:44 +0530

    Logic:Create printBooks and callback addToBooksList

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ function addBookToLibrary(name) {
   const newBook = new Book(name);
   myLibrary.push(newBook);
 }
+
+const books = document.getElementById("books");
+
+function AddToBooksList(bookName) {
+  const book = document.createElement("li");
+  const title = document.createTextNode(bookName);
+  book.appendChild(title);
+  books.appendChild(book);
+}
+
+function printBooks() {
+  myLibrary.forEach(AddToBooksList);
+}
